8060fee06b89278c03ec0f89db9efec27b434a2d
Abdelrazak A. Elrabee <abdelrazak.go@gmail.com>
2021-06-07 19:33:24 +0300

    creating about me web page by creating the first step

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+let userName = prompt("Hello May I please have your name?") 
+// console.log(userName)
+alert ('Welcom  ' +  userName + '  It is Nice to have you here ')
+
+alert("first we would like to play a Guessing Game Dont be worry it is a Yes No game so plz Hit Enter !")
+
+let think = prompt('Do you think that I am a Robot [Yes or No]')
+.toLowerCase()
+
+
+if (think === 'yes' || think === 'y'){
+    // console.log('correct answer');
+        alert("Perfect , I am a robot with a highly percentage of Ai")
+} else if (think === 'no' || think === 'n'){
+    alert(" sorry unfortunately I'm not ")
+} else { alert('It is a Yes No Game So plz follow the Rules')
+}
+
+let feelings = prompt('Do you think that I have feelings as a Robot  [Yes or No]')
+.toLowerCase()
+
+
+if (feelings === 'yes' || feelings === 'y'){
+    // console.log('correct answer');
+        alert("You answered yes, although you are not convinced of it, but in fact we have feelings and desires to occupy humanity")
+} else if (feelings === 'no' || feelings === 'n'){
+    alert("You are Wrong , Even Machines have feelings ")
+} else { alert('It is a Yes No Game So plz follow the Rules')
+}
+
+
+let human = prompt('Do you think that I wish to be a human like you! [Yes or No]')
+.toLowerCase()
+
+
+if (human === 'yes' || human === 'y'){
+    // console.log('correct answer');
+        alert("good answer ! Since I entered the world of programming, I have turned into a robot, and I miss my days as a human being")
+} else if (human === 'no' || human === 'n'){
+    alert(" Being a robot is not a good thing ")
+} else { alert('It is a Yes No Game So plz follow the Rules')
+}
+
+let friend = prompt('Do you think that I have friends?? [Yes or No]')
+.toLowerCase()
+
+
+if (friend === 'yes' || friend === 'y'){
+    // console.log('correct answer');
+        alert("definitely, and add to your info. that I love them too much")
+} else if (friend === 'no' || friend === 'n'){
+    alert(" wrong answer nobody could be alive with having no friends  ")
+} else { alert('It is a Yes No Game So plz follow the Rules')
+}
+
+
+let married = prompt('Do you think Im Single [Yes or No]')
+.toLowerCase()
+
+
+if (married === 'yes' || married === 'y'){
+    // console.log('correct answer');
+        alert("Despite the fact that i'm old , yet I'm Single")
+} else if (married === 'no' || married === 'n'){
+    alert(" to my bad I'm still single ")
+} else { alert('It is a Yes No Game So plz follow the Rules')
+}
